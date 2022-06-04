c24e9e1c6df41e67b4002f291c981f593a212608
Aluno <pedro.ghiotti.m@gmail.com>
2022-06-03 22:29:00 -0300

    Flocking etapa 2

--- IA_Flocking/Assets/_Scripts/Flock.cs
+++ IA_Flocking/Assets/_Scripts/Flock.cs
@@ -7,6 +7,8 @@ public class Flock : MonoBehaviour
     public FlockManager flockManager;
     float speed;
 
+    Vector3 groupCentre;
+
     private void Start()
     {
         // Atribui à variável ´speed´ um valor aleatório entre os valores mínimo e máximo determinados no FlockManager.
@@ -14,7 +16,69 @@ public class Flock : MonoBehaviour
     }
     private void Update()
     {
-        // Move o objeto no eixo Z em determinada velocidade.
+        ApplyRules();
+
+        // Move o objeto no eixo Z (Local) em determinada velocidade.
         transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    private void ApplyRules()
+    {
+        GameObject[] arrayGameObjects = flockManager.allFish;
+
+        Vector3 posFlockCentre = Vector3.zero;
+        Vector3 vAvoid = Vector3.zero;
+
+        float groupSpeed = 0.01f;
+        float distanceToOther;
+        float groupSize = 0;
+
+        foreach (GameObject fish in arrayGameObjects)
+        {
+            if (fish != this.gameObject)
+            {
+                distanceToOther = Vector3.Distance(fish.transform.position, this.transform.position);
+
+                if (distanceToOther <= flockManager.neighbourDistance)
+                {
+                    posFlockCentre += fish.transform.position;
+                    groupSize++;
+
+                    if (distanceToOther < 1f)
+                    {
+                        vAvoid = vAvoid + (this.transform.position - fish.transform.position);
+                    }
+
+                    Flock flockOtherFish = fish.GetComponent<Flock>();
+                    groupSpeed = groupSpeed + flockOtherFish.speed;
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            posFlockCentre = posFlockCentre / groupSize;
+            speed = groupSpeed / groupSize;
+
+            Vector3 direction = (posFlockCentre + vAvoid) - this.transform.position;
+
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp
+                (
+                    transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    flockManager.rotationSpeed * Time.deltaTime
+                );
+            }
+        }
+
+        groupCentre = posFlockCentre;
+        print(Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groupCentre, .5f);
     }
 }
--- IA_Flocking/Assets/_Scripts/FlockManager.cs
+++ IA_Flocking/Assets/_Scripts/FlockManager.cs
@@ -10,20 +10,28 @@ public class FlockManager : MonoBehaviour
     public Vector3 swimLimits = new Vector3(5, 5, 5);
 
     [Header("Configurações do Cardume")]
-    [Range(0, 5f)] public float minSpeed;
-    [Range(0, 5f)] public int maxSpeed;
+    [Range(0, 5f)]  public float minSpeed;
+    [Range(0, 5f)]  public int maxSpeed;
+    [Range(1, 10f)] public float neighbourDistance;
+    [Range(0, 5f)]  public float rotationSpeed;
 
     private void Start()
     {
-        // Inicializa o array ´allFish´ com o tamanho determinado na variavel ´numFish´.
+        // Inicializa o array 'allFish' com o tamanho determinado na variavel 'numFish'.
         allFish = new GameObject[numFish];
 
-        // Popula o array ´numFish´ com instâncias criadas da prefab ´fishPrefab´ em posições aleatórias dentro dos limites definidos em ´swimLimits´ e 
+        /*
+            Popula o array 'numFish' com instâncias da prefab 'fishPrefab' em posições aleatórias dentro dos limites definidos em 'swimLimits'.
+            Atribui esse objeto à 'flockManager' do componente 'Flock' de cada objeto instanciado.
+        */
         for (int i = 0; i < numFish; i++)
         {
-            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
-                                                                Random.Range(-swimLimits.y, swimLimits.y),
-                                                                Random.Range(-swimLimits.z, swimLimits.z));
+            Vector3 pos = this.transform.position + new Vector3
+            (
+                Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.y, swimLimits.y),
+                Random.Range(-swimLimits.z, swimLimits.z)
+            );
 
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             allFish[i].GetComponent<Flock>().flockManager = this;
